c344a43c69f53543abe0d72a0a4b3f04d7e0a1af
z-chu <z-chu@qq.com>
2016-10-25 23:00:09 +0800

    添加一个简单的例子

--- rxcache/src/main/java/com/zchu/rxcache/stategy/CacheStrategy.java
+++ rxcache/src/main/java/com/zchu/rxcache/stategy/CacheStrategy.java
@@ -6,18 +6,33 @@ package com.zchu.rxcache.stategy;
 
 public final class CacheStrategy {
 
+    /**
+     * 优先网络
+     */
     public static IStrategy firstRemote(){
         return  FirstRemoteStrategy.INSTANCE;
     }
+    /**
+     * 优先缓存
+     */
     public static IStrategy firstCache(){
         return  FirstCacheStategy.INSTANCE;
     }
+    /**
+     * 仅加载网络，但数据依然会被缓存
+     */
     public static IStrategy  onlyRemote(){
         return  OnlyRemoteStrategy.INSTANCE;
     }
+    /**
+     * 仅加载缓存
+     */
     public static IStrategy onlyCache(){
         return  OnlyCacheStrategy.INSTANCE;
     }
+    /**
+     * 先加载缓存，后加载网络
+     */
     public static IStrategy cacheAndRemote(){
         return  CacheAndRemoteStrategy.INSTANCE;
     }
